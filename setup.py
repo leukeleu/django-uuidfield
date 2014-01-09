3edd6b288e047cbf6bbaf18d31b4d10c06b43718
Marco Braak <mbraak@ridethepony.nl>
2014-01-09 16:02:31 +0100

    Updated version to 0.4.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-uuidfield',
-    version='0.4.0.2',
+    version='0.4.0.3',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     description='UUIDField in Django',
